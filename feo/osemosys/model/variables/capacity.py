1408d92c218e534fb0b899cd14b6d2a7afb27ddd
Lucas Kruitwagen <lucas.kruitwagen@gmail.com>
2023-10-26 12:10:06 +0100

    feat: linopy boilerplate (#2)

---
+++ feo/osemosys/model/variables/capacity.py
@@ -0,0 +1,27 @@
+def add(ds: xr.Dataset, m: Model) -> Model:
+    """Add capacity variables to the model
+
+    Arguments
+    ---------
+    ds: xarray.Dataset
+        The parameters dataset
+    m: linopy.Model
+        A linopy model
+
+    Returns
+    -------
+    linopy.Model
+    """
+    # Create the required index
+    RTeY = [ds.coords["REGION"], ds.coords["TECHNOLOGY"], ds.coords["YEAR"]]
+
+    # masks
+    mask = ds["CapacityOfOneTechnologyUnit"].notnull()
+
+    m.add_variables(
+        lower=0, upper=inf, coords=RTeY, name="NumberOfNewTechnologyUnits", integer=True, mask=mask
+    )
+    m.add_variables(lower=0, upper=inf, coords=RTeY, name="NewCapacity", integer=False)
+    m.add_variables(lower=0, upper=inf, coords=RTeY, name="AccumulatedNewCapacity", integer=False)
+    m.add_variables(lower=0, upper=inf, coords=RTeY, name="TotalCapacityAnnual", integer=False)
+    return m
---
+++ feo/osemosys/model/variables/demand.py
@@ -0,0 +1,27 @@
+import xarray as xr
+from linopy import Model
+from numpy import inf
+
+# object oriented?
+
+
+def add(ds: xr.Dataset, m: Model) -> Model:
+    """Add demand variables to the model
+
+    Arguments
+    ---------
+    ds: xarray.Dataset
+        The parameters dataset
+    m: linopy.Model
+        A linopy model
+
+    Returns
+    -------
+    linopy.Model
+    """
+    RTiFY = [ds.coords["REGION"], ds.coords["TIMESLICE"], ds.coords["FUEL"], ds.coords["YEAR"]]
+
+    # mask = ds['SpecifiedAnnualDemand'].expand_dims('TIMESLICE').notnull()
+    m.add_variables(lower=0, upper=inf, coords=RTiFY, name="RateOfDemand", integer=False)
+    m.add_variables(lower=0, upper=inf, coords=RTiFY, name="Demand", integer=False)
+    return m
