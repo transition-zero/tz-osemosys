071ffad3aa27f98f8d97fa26dc0ba38b0a4e37f8
edwardxtg <edwardxtg@gmail.com>
2023-11-20 14:33:06 +0000

    refactor schema fuel

--- feo/osemosys/schemas/fuel.py
+++ feo/osemosys/schemas/fuel.py
@@ -1,32 +1,70 @@
 import os
 
 import pandas as pd
+from pydantic import BaseModel, conlist, root_validator
+from typing import ClassVar
+from pathlib import Path
 
 from feo.osemosys.utils import *
 
 from .base import *
 
 
+class OtooleCfg(BaseModel):
+    """
+    Paramters needed to round-trip csvs from otoole
+    """
+
+    empty_dfs: List[str] | None
+
 class Fuel(OSeMOSYSBase):
     # either demand (region:year:timeslice:value)
     # or annual_demand (region:year:value) must be specified;
     # SpecifiedDemandProfile may be optionally specified with annual_demand
-    SpecifiedAnnualDemand: RegionYearData | None
-    SpecifiedDemandProfile: RegionYearTimeData | None
-    AccumulatedAnnualDemand: RegionYearData | None
-    RETagFuel: RegionYearData | None  # why would this change over time??
+    SpecifiedAnnualDemand: OSeMOSYSData | None
+    SpecifiedDemandProfile: OSeMOSYSData | None
+    AccumulatedAnnualDemand: OSeMOSYSData | None
+    RETagFuel: OSeMOSYSData | None  # why would this change over time??
+
+    otoole_cfg: OtooleCfg | None
+    otoole_stems: ClassVar[list[str]] = [
+        "SpecifiedAnnualDemand",
+        "SpecifiedDemandProfile",
+        "AccumulatedAnnualDemand",
+        "RETagFuel",
+    ]
+
+    @root_validator(pre=True)
+    def construct_from_components(cls, values):
+        CapacityTSpecifiedAnnualDemandoActivityUnit = values.get("SpecifiedAnnualDemand")
+        SpecifiedDemandProfile = values.get("SpecifiedDemandProfile")
+        AccumulatedAnnualDemand = values.get("AccumulatedAnnualDemand")
+        RETagFuel = values.get("RETagFuel")
+
+        return values
 
     @classmethod
     def from_otoole_csv(cls, root_dir) -> List["cls"]:
-        #
+        
+        # ###########
+        # Load Data #
+        # ###########
+
         df_fuel = pd.read_csv(os.path.join(root_dir, "FUEL.csv"))
-        df_annual_demand = pd.read_csv(os.path.join(root_dir, "SpecifiedAnnualDemand.csv"))
-        df_SpecifiedDemandProfile = pd.read_csv(os.path.join(root_dir, "SpecifiedDemandProfile.csv"))
-        df_AccumulatedAnnualDemand = pd.read_csv(os.path.join(root_dir, "AccumulatedAnnualDemand.csv"))
-        df_RETagFuel = pd.read_csv(os.path.join(root_dir, "RETagFuel.csv"))
+        
+        dfs = {}
+        otoole_cfg = OtooleCfg(empty_dfs=[])
+        for key in cls.otoole_stems:
+            try:
+                dfs[key] = pd.read_csv(Path(root_dir) / f"{key}.csv")
+                if dfs[key].empty:
+                    otoole_cfg.empty_dfs.append(key)
+            except FileNotFoundError:
+                otoole_cfg.empty_dfs.append(key)
+                dfs[key] = pd.DataFrame(columns=["FUEL"])
 
         assert (
-            (df_SpecifiedDemandProfile.groupby(["REGION", "FUEL", "YEAR"])["VALUE"].sum() == 1.0).all(),
+            (dfs["SpecifiedDemandProfile"].groupby(["REGION", "FUEL", "YEAR"])["VALUE"].sum() == 1.0).all(),
             "demand profiles must sum to one for all REGION, FUEL, and YEAR",
         )
 
@@ -38,54 +76,55 @@ class Fuel(OSeMOSYSBase):
                     # TODO
                     long_name=None,
                     description=None,
+                    otoole_cfg=otoole_cfg,
                     SpecifiedAnnualDemand=(
-                        RegionYearData(
+                        OSeMOSYSData(
                             data=group_to_json(
-                                g=df_annual_demand.loc[df_annual_demand["FUEL"] == fuel],
+                                g=dfs["SpecifiedAnnualDemand"].loc[dfs["SpecifiedAnnualDemand"]["FUEL"] == fuel],
                                 root_column="FUEL",
                                 data_columns=["REGION", "YEAR"],
                                 target_column="VALUE",
                             )
                         )
-                        if fuel in df_annual_demand["FUEL"].values
+                        if fuel in dfs["SpecifiedAnnualDemand"]["FUEL"].values
                         else None
                     ),
                     SpecifiedDemandProfile=(
-                        RegionYearTimeData(
+                        OSeMOSYSData(
                             data=group_to_json(
-                                g=df_SpecifiedDemandProfile.loc[df_SpecifiedDemandProfile["FUEL"] == fuel],
+                                g=dfs["SpecifiedDemandProfile"].loc[dfs["SpecifiedDemandProfile"]["FUEL"] == fuel],
                                 root_column="FUEL",
                                 data_columns=["REGION", "TIMESLICE", "YEAR"],
                                 target_column="VALUE",
                             )
                         )
-                        if fuel in df_SpecifiedDemandProfile["FUEL"].values
+                        if fuel in dfs["SpecifiedDemandProfile"]["FUEL"].values
                         else None
                     ),
                     AccumulatedAnnualDemand=(
-                        RegionYearData(
+                        OSeMOSYSData(
                             data=group_to_json(
-                                g=df_AccumulatedAnnualDemand.loc[
-                                    df_AccumulatedAnnualDemand["FUEL"] == fuel
+                                g=dfs["AccumulatedAnnualDemand"].loc[
+                                    dfs["AccumulatedAnnualDemand"]["FUEL"] == fuel
                                 ],
                                 root_column="FUEL",
                                 data_columns=["REGION", "YEAR"],
                                 target_column="VALUE",
                             )
                         )
-                        if fuel in df_AccumulatedAnnualDemand["FUEL"].values
+                        if fuel in dfs["AccumulatedAnnualDemand"]["FUEL"].values
                         else None
                     ),
                     RETagFuel=(
-                        RegionYearData(
+                        OSeMOSYSData(
                             data=group_to_json(
-                                g=df_RETagFuel.loc[df_RETagFuel["FUEL"] == fuel],
+                                g=dfs["RETagFuel"].loc[dfs["RETagFuel"]["FUEL"] == fuel],
                                 root_column="FUEL",
                                 data_columns=["REGION", "YEAR"],
                                 target_column="VALUE",
                             )
                         )
-                        if fuel in df_RETagFuel["FUEL"].values
+                        if fuel in dfs["RETagFuel"]["FUEL"].values
                         else None
                     ),
                 )
