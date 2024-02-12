532ca8419d585c6444115d93c42c02418e94bfc6
Lucas Kruitwagen <lucas.kruitwagen@gmail.com>
2024-02-12 01:39:52 +0000

    feat: import utils by name

--- feo/osemosys/utils/__init__.py
+++ feo/osemosys/utils/__init__.py
@@ -1,2 +1,21 @@
-from feo.osemosys.utils.cfg_parser import *  # noqa: F401,F403
-from feo.osemosys.utils.utils import *  # noqa: F401,F403
+from feo.osemosys.utils.cfg_parser import EnvVarLoader, substitute_factory, walk_dict
+from feo.osemosys.utils.utils import (
+    flatten,
+    group_to_json,
+    json_dict_to_dataframe,
+    makehash,
+    maybe_eval_string,
+    merge,
+)
+
+__all__ = [
+    "EnvVarLoader",
+    "walk_dict",
+    "substitute_factory",
+    "maybe_eval_string",
+    "merge",
+    "flatten",
+    "makehash",
+    "group_to_json",
+    "json_dict_to_dataframe",
+]
