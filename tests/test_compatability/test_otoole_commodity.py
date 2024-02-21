147c1ea3a6f64fd290402ffc66087c16d3e4a262
Lucas Kruitwagen <lucas.kruitwagen@gmail.com>
2024-02-21 03:01:05 +0000

    tests: commodity construction and compatability

---
+++ tests/test_compatability/test_otoole_commodity.py
@@ -0,0 +1,41 @@
+from pathlib import Path
+
+import pandas as pd
+
+from feo.osemosys.schemas.commodity import Commodity
+from tests.fixtures.paths import OTOOLE_SAMPLE_PATHS
+
+
+def test_otoole_roundtrip():
+    comparison_root = "./tests/otoole_compare/"
+
+    for path in OTOOLE_SAMPLE_PATHS:
+        comparison_model = Path(path).name
+        output_directory = Path(comparison_root + comparison_model)
+        output_directory.mkdir(parents=True, exist_ok=True)
+
+        commodities = Commodity.from_otoole_csv(root_dir=path)
+
+        Commodity.to_otoole_csv(commodities=commodities, output_directory=output_directory)
+
+        for stem, params in commodities[0].otoole_stems.items():
+            if stem not in commodities[0].otoole_cfg.empty_dfs:
+                left = (
+                    pd.read_csv(Path(path) / (stem + ".csv"))
+                    .sort_values(params["columns"])
+                    .reset_index(drop=True)
+                    .sort_index(axis=1)
+                )
+                right = (
+                    pd.read_csv(Path(output_directory) / Path(stem + ".csv"))
+                    .sort_values(params["columns"])
+                    .reset_index(drop=True)
+                    .sort_index(axis=1)
+                )
+                print("LEFT")
+                print(left)
+                print("RIGHT")
+                print(right)
+                print("ASSERT", stem, left.equals(right))
+
+                assert left.equals(right)
---
+++ tests/test_construction/test_commodity.py
@@ -0,0 +1,38 @@
+import pytest
+
+from feo.osemosys.schemas.commodity import Commodity
+
+PASSING_COMMODITY_DEFINITIONS = dict(
+    super_basic_no_demand=dict(id="WATER"),
+    super_basic_accumulated_demand=dict(id="COAL", demand_annual=100.0),
+    super_basic_demand_profile=dict(id="coal", demand_annual=5, demand_profile=1.0),
+    basic_demand_profile=dict(
+        id="coal",
+        demand_annual=5,
+        demand_profile={"*": {"0h": 0.0, "6h": 0.2, "12h": 0.3, "18h": 0.5}},
+    ),
+)
+
+FAILING_COMMODITY_DEFINITIONS = dict(
+    demand_profile_no_annual=dict(
+        id="coal", demand_profile={"*": {"0h": 0.0, "6h": 0.2, "12h": 0.3, "18h": 0.5}}
+    ),
+    demand_profile_gt_one=dict(
+        id="coal",
+        demand_annual=5,
+        demand_profile={"*": {"0h": 0.4, "6h": 0.2, "12h": 0.3, "18h": 0.5}},
+    ),
+)
+
+
+def test_commodity_construction():
+    for _name, params in PASSING_COMMODITY_DEFINITIONS.items():
+        commod = Commodity(**params)
+        assert isinstance(commod, Commodity)
+
+
+def test_region_construction_failcases():
+    for _name, params in FAILING_COMMODITY_DEFINITIONS.items():
+        print(_name)
+        with pytest.raises(ValueError) as e:  # noqa: F841
+            Commodity(**params)
