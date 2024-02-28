26bb0decf364ea6cd393ec6a0cac5d715a844354
edwardxtg <edwardxtg@gmail.com>
2024-02-28 02:25:18 +0000

    additional fail cases test_compose_base.py

--- tests/test_composition/test_compose_base.py
+++ tests/test_composition/test_compose_base.py
@@ -84,6 +84,42 @@ FAILING_COMMODITIES = dict(
     ),
 )
 
+FAILING_TECHNOLOGIES = dict(
+    non_matching_commodities=dict(
+        params=dict(
+            id="most_basic",
+            operating_life=10,
+            capex=15,
+            opex_fixed=1.5,
+            operating_modes=[
+                dict(id="mode_1", opex_variable=1.5, input_activity_ratio={"COAL": 1.0})
+            ],
+        ),
+        sets=dict(
+            regions=["R1", "R2"],
+            years=[2025, 2026, 2027],
+            timeslices=["0h", "6h", "12h", "18h"],
+            commodities=["GAS"],
+        ),
+    )
+)
+
+FAILING_IMPACTS = dict(
+    too_deep_nesting=dict(
+        params=dict(
+            id="CO2e",
+            constraint_annual={"*": {"*": {"*": 1}}},
+            constraint_total={"*": {"*": 10}},
+        ),
+        sets=dict(
+            regions=["R1", "R2"],
+            years=[2025, 2026, 2027],
+            timeslices=["0h", "6h", "12h", "18h"],
+            commodities=["COAL"],
+        ),
+    )
+)
+
 
 def test_compose_impact():
     for _name, data in PASSING_IMPACTS.items():
@@ -137,3 +173,17 @@ def test_failing_commodity():
         with pytest.raises(ValueError) as e:  # noqa: F841
             commodity = Commodity(**data["params"])
             commodity.compose(**data["sets"])
+
+
+def test_failing_technology():
+    for _name, data in FAILING_TECHNOLOGIES.items():
+        with pytest.raises(ValueError) as e:  # noqa: F841
+            technology = Technology(**data["params"])
+            technology.compose(**data["sets"])
+
+
+def test_failing_impact():
+    for _name, data in FAILING_IMPACTS.items():
+        with pytest.raises(ValueError) as e:  # noqa: F841
+            impact = Impact(**data["params"])
+            impact.compose(**data["sets"])
