c28b83d1f6f273c3ec2405ac38bd7d7870efafb0
Abhishek  Shivakumar <adminuser@MacBook-Pro-7.local>
2024-03-07 17:35:04 +0000

    test: Added full linopy run integration test

--- tests/fixtures/paths.py
+++ tests/fixtures/paths.py
@@ -5,3 +5,8 @@ OTOOLE_SAMPLE_PATHS = [
     "examples/otoole_csvs/otoole-simple-hydro",
     "examples/otoole_csvs/otoole-full-electricity",
 ]
+
+OTOOLE_SAMPLE_RESULTS = [
+    "examples/otoole_results/otoole-simple-hydro",
+    "examples/otoole_results/otoole-full-electricity",
+]
