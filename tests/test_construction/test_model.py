7bf96cc8a011ae041030eb8a6f93ec2c1f3abac8
Lucas Kruitwagen <lucas.kruitwagen@gmail.com>
2024-03-09 17:17:53 +0000

    tests: construction tests for model and technology

--- tests/test_construction/test_model.py
+++ tests/test_construction/test_model.py
@@ -5,18 +5,19 @@ import pytest
 from feo.osemosys.schemas.model import RunSpec
 
 
-@pytest.mark.skip(reason="implemented upstream")
 def test_model_construction():
-    run_spec_object = RunSpec.from_otoole(root_dir="examples/otoole_csvs/otoole-full-electricity/")
+    run_spec_object = RunSpec.from_otoole_csv(
+        root_dir="examples/otoole_csvs/otoole-full-electricity/"
+    )
     assert isinstance(run_spec_object, RunSpec)
 
 
-@pytest.mark.skip(reason="implemented upstream")
+@pytest.mark.skip(reason="fail cases not implemented?")
 def test_model_construction_failcases(create_test_model_construction_failcases):
     folder_paths = create_test_model_construction_failcases
     with pytest.raises(ValueError) as e:  # noqa: F841
         for csv_folder in folder_paths:
-            RunSpec.from_otoole(root_dir=csv_folder)
+            RunSpec.from_otoole_csv(root_dir=csv_folder)
     # Delete folders once used
     for csv_folder in folder_paths:
         shutil.rmtree(csv_folder)
