784bb260e7316554d1f8afdfcc0562083bf6f34d
edwardxtg <edwardxtg@gmail.com>
2024-03-20 14:18:27 +0000

    Region docstring

--- tz/osemosys/schemas/region.py
+++ tz/osemosys/schemas/region.py
@@ -16,11 +16,47 @@ from tz.osemosys.schemas.validation.region_validation import (
 
 class Region(OSeMOSYSBase, OtooleRegion):
     """
-    Class to contain all information pertaining to regions and trade routes including:
-    - discount rates
-    - reserve margins
-    - renewable production targets
-    - depreciation method
+    # Region
+
+    The Region class contains data related to regions, including neighbours and trade routes.
+
+    ## Parameters
+
+    `id` `(str)`: Used to represent the region name.
+
+    `neighbours` `(List[str])`: Used to represent the impact name. Optional, defaults to
+    `None`.
+
+    `trade_routes` `({region:{commodity:{year:bool}}})` - Boolean tag indicating which other regions
+    may trade the specified commodities with the current region. Optional, defaults to
+    `None`.
+
+    ## Examples
+
+    A simple example of how a region ("R1") might be defined as able to trade the commodity "COAL"
+    with region "R2" is given below, along with how it can be used to create an instance of the
+    Region class:
+
+    ```python
+    from tz.osemosys.schemas.region import Region
+
+    basic_region = dict(
+        id="R1",
+        trade_routes={"R2": {"COAL": {"2020": True, "2021": True, "2022": True}}},
+    )
+
+    Region(**basic_region)
+    ```
+
+    This model can be expressed more simply using the wildcard `*` for dimensions over which data is
+    repeated:
+
+    ```python
+    basic_region = dict(
+        id="R1",
+        trade_routes={"R2": {"COAL": {"*": True}}},
+    )
+    ```
     """
 
     model_config = ConfigDict(extra="forbid")
