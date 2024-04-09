e4ffd0cbbc7a99d6517d49400f0993cd03b00e73
edwardxtg <edwardxtg@gmail.com>
2024-04-09 17:25:29 +0100

    only add EMISSION linear exp if necessary

--- tz/osemosys/model/linear_expressions/__init__.py
+++ tz/osemosys/model/linear_expressions/__init__.py
@@ -18,10 +18,12 @@ def add_linear_expressions(ds: xr.Dataset, m: Model) -> Dict[str, LinearExpressi
 
     add_lex_activity(ds, m, lex)
     add_lex_capacity(ds, m, lex)
-    add_lex_emissions(ds, m, lex)
+    if ds["EMISSION"].size > 0:
+        add_lex_emissions(ds, m, lex)
     add_lex_financials(ds, m, lex)
     add_lex_quantities(ds, m, lex)
     add_lex_reserve_margin(ds, m, lex)
-    # add_lex_storage(ds, m, lex)
+    # if ds["STORAGE"].size > 0:
+    #   add_lex_storage(ds, m, lex)
 
     return lex
