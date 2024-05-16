c035b543dc3feefb2227b697d6ab6863351dd7f6
Thomas Kouroughli <tomkourou@icloud.com>
2024-05-16 12:49:33 +0100

    chore(validation): 🚨 fix linting

--- tz/osemosys/schemas/validation/validation_utils.py
+++ tz/osemosys/schemas/validation/validation_utils.py
@@ -7,9 +7,7 @@ from tz.osemosys.utils import json_dict_to_dataframe
 def check_sums_one(data, leniency, cols, cols_to_groupby):
     data_df = pd.json_normalize(data)
     data_df.columns = cols
-    assert np.allclose(
-        data_df.groupby(cols_to_groupby)["VALUE"].sum(), 1, atol=leniency
-    ), (
+    assert np.allclose(data_df.groupby(cols_to_groupby)["VALUE"].sum(), 1, atol=leniency), (
         f"demand_profile must sum to one (within {leniency}) for all REGION,"
         f" YEAR, and commodity; commodity {id} does not"
     )
