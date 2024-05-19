824ef777553f3bd102cc80dd8285ae4b39b5af33
Lucas Kruitwagen <lucas.kruitwagen@gmail.com>
2024-05-19 22:05:37 +0100

    feat: make storage linexpr using stacked (time,year)

--- tz/osemosys/model/linear_expressions/storage.py
+++ tz/osemosys/model/linear_expressions/storage.py
@@ -10,91 +10,36 @@ def add_lex_storage(ds: xr.Dataset, m: Model, lex: Dict[str, LinearExpression]):
         1 + ds.coords["YEAR"][-1] - ds.coords["YEAR"][0]
     )
 
-    ConversionFactor = ds["Conversionlh"] * ds["Conversionld"] * ds["Conversionls"]
+    ds["Conversionlh"] * ds["Conversionld"] * ds["Conversionls"]
 
     RateOfStorageCharge = (
-        (ConversionFactor * ds["TechnologyToStorage"] * m["RateOfActivity"]).where(
-            (ds["TechnologyToStorage"].notnull())
-            & (ds["TechnologyToStorage"] != 0)
-            & (ConversionFactor).notnull()
+        (ds["TechnologyToStorage"] * m["RateOfActivity"]).where(
+            (ds["TechnologyToStorage"].notnull()) & (ds["TechnologyToStorage"] != 0)
         )
-    ).sum(["TECHNOLOGY", "MODE_OF_OPERATION", "TIMESLICE"])
+    ).sum(["TECHNOLOGY", "MODE_OF_OPERATION"])
 
     RateOfStorageDischarge = (
-        (ConversionFactor * ds["TechnologyFromStorage"] * m["RateOfActivity"]).where(
-            (ds["TechnologyFromStorage"].notnull())
-            & (ds["TechnologyFromStorage"] != 0)
-            & (ConversionFactor).notnull()
+        (ds["TechnologyFromStorage"] * m["RateOfActivity"]).where(
+            (ds["TechnologyFromStorage"].notnull()) & (ds["TechnologyFromStorage"] != 0)
         )
-    ).sum(["TECHNOLOGY", "MODE_OF_OPERATION", "TIMESLICE"])
+    ).sum(["TECHNOLOGY", "MODE_OF_OPERATION"])
 
-    NetChargeWithinYear = (
-        (RateOfStorageCharge - RateOfStorageDischarge).where(ConversionFactor.notnull())
-        * ConversionFactor
-        * ds["YearSplit"]
-    ).sum("TIMESLICE")
+    NetCharge = RateOfStorageCharge - RateOfStorageDischarge
 
-    NetChargeWithinDay = (RateOfStorageCharge - RateOfStorageDischarge) * ds["DaySplit"]
+    NetChargeReshape = NetCharge.stack(YRTS=["YEAR", "TIMESLICE"])
 
-    # YEAR
-    firstyear_mask = ds["YEAR"] == ds["YEAR"][0]
-    # TODO add StorageLevelStart rather than 0 for the first year
-    # ds["StorageLevelStart"].expand_dims(YEAR=ds["YEAR"].values).where(firstyear_mask)
-    StorageLevelYearStart = 0 + (
-        m["StorageLevelYearStart"].shift(YEAR=1)
-        + NetChargeWithinYear.shift(YEAR=1).sum(["SEASON", "DAYTYPE", "DAILYTIMEBRACKET"])
-    ).where(~firstyear_mask)
-
-    notlastyear_mask = ds["YEAR"] < ds["YEAR"][-1]
-    StorageLevelYearFinish = StorageLevelYearStart.shift(YEAR=-1).where(notlastyear_mask) + (
-        StorageLevelYearStart + NetChargeWithinYear.sum(["SEASON", "DAYTYPE", "DAILYTIMEBRACKET"])
-    ).where(~notlastyear_mask)
-
-    # SEASON
-    firstseason_mask = ds["SEASON"] == ds["SEASON"][0]
-    StorageLevelSeasonStart = StorageLevelYearStart.where(firstseason_mask) + (
-        m["StorageLevelSeasonStart"].shift(SEASON=1)
-        + NetChargeWithinYear.shift(SEASON=1).sum(["DAYTYPE", "DAILYTIMEBRACKET"])
-    ).where(~firstseason_mask)
-
-    # DAYTYPE
-    firstdaytype_mask = ds["DAYTYPE"] == ds["DAYTYPE"][0]
-    StorageLevelDayTypeStart = StorageLevelSeasonStart.where(firstdaytype_mask) + (
-        m["StorageLevelDayTypeStart"].shift(DAYTYPE=1)
-        + (
-            (NetChargeWithinDay.shift(DAYTYPE=1) * ds["DaysInDayType"].shift(DAYTYPE=1)).sum(
-                ["DAILYTIMEBRACKET"]
-            )
-        )
-    ).where(~firstdaytype_mask)
-
-    lastseason_mask = ds["SEASON"] == ds["SEASON"][-1]
-    lastdaytype_mask = ds["DAYTYPE"] == ds["DAYTYPE"][-1]
-
-    StorageLevelDayTypeFinish = (
-        (StorageLevelYearFinish.where((lastseason_mask) & (lastdaytype_mask)))
-        + (StorageLevelSeasonStart.shift(SEASON=-1).where((lastdaytype_mask) & (~lastseason_mask)))
-        + (
-            m["StorageLevelDayTypeFinish"].shift(DAYTYPE=-1)
-            - (NetChargeWithinDay.shift(DAYTYPE=-1) * ds["DaysInDayType"].shift(DAYTYPE=-1)).sum(
-                ["DAILYTIMEBRACKET"]
-            )
-        ).where(
-            ~((lastseason_mask) & (lastdaytype_mask)) & ~((lastdaytype_mask) & (~lastseason_mask))
-        )
-    )
-
-    StorageUpperLimit = m["AccumulatedNewStorageCapacity"] + ds["ResidualStorageCapacity"]
-
-    StorageLowerLimit = ds["MinStorageCharge"] * StorageUpperLimit
+    StorageLevel = NetChargeReshape.cumsum("YRTS") + ds.get("StorageLevelStart", 0.0)
 
     NewStorageCapacity = m["NewStorageCapacity"].rename(YEAR="BUILDYEAR")
 
+    # mask to handle operating life of storage
     mask = (ds.YEAR - NewStorageCapacity.data.BUILDYEAR >= 0) & (
         ds.YEAR - NewStorageCapacity.data.BUILDYEAR < ds.OperationalLifeStorage
     )
 
     AccumulatedNewStorageCapacity = NewStorageCapacity.where(mask).sum("BUILDYEAR")
+
+    GrossStorageCapacity = AccumulatedNewStorageCapacity + ds["ResidualStorageCapacity"]
 
     CapitalInvestmentStorage = ds["CapitalCostStorage"] * m["NewStorageCapacity"]
     DiscountedCapitalInvestmentStorage = CapitalInvestmentStorage / lex["DiscountFactor"]
@@ -120,17 +65,11 @@ def add_lex_storage(ds: xr.Dataset, m: Model, lex: Dict[str, LinearExpression]):
         {
             "RateOfStorageCharge": RateOfStorageCharge,
             "RateOfStorageDischarge": RateOfStorageDischarge,
-            "NetChargeWithinYear": NetChargeWithinYear,
-            "NetChargeWithinDay": NetChargeWithinDay,
-            "StorageLevelYearStart": StorageLevelYearStart,
-            "StorageLevelYearFinish": StorageLevelYearFinish,
-            "StorageLevelSeasonStart": StorageLevelSeasonStart,
-            "StorageLevelDayTypeStart": StorageLevelDayTypeStart,
-            "StorageLevelDayTypeFinish": StorageLevelDayTypeFinish,
-            "StorageUpperLimit": StorageUpperLimit,
-            "StorageLowerLimit": StorageLowerLimit,
+            "NetCharge": NetCharge,
+            "StorageLevel": StorageLevel,
             "NewStorageCapacity": NewStorageCapacity,
             "AccumulatedNewStorageCapacity": AccumulatedNewStorageCapacity,
+            "GrossStorageCapacity": GrossStorageCapacity,
             "CapitalInvestmentStorage": CapitalInvestmentStorage,
             "DiscountedCapitalInvestmentStorage": DiscountedCapitalInvestmentStorage,
             "TotalDiscountedStorageCost": TotalDiscountedStorageCost,
