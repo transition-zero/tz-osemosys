f6c2d8740317b9c8a2187c70df43f0d910408693
Lucas Kruitwagen <lucas.kruitwagen@gmail.com>
2024-05-19 22:02:26 +0100

    feat: PURGE storage variables

--- tz/osemosys/model/variables/storage.py
+++ tz/osemosys/model/variables/storage.py
@@ -19,47 +19,5 @@ def add_storage_variables(ds: xr.Dataset, m: Model) -> Model:
     """
     RSY = [ds.coords["REGION"], ds.coords["STORAGE"], ds.coords["YEAR"]]
 
-    m.add_variables(lower=0, upper=inf, coords=RSY, name="StorageLevelYearStart", integer=False)
-    m.add_variables(lower=0, upper=inf, coords=RSY, name="StorageLevelYearFinish", integer=False)
-    coords = [ds.coords["REGION"], ds.coords["STORAGE"], ds.coords["SEASON"], ds.coords["YEAR"]]
-    m.add_variables(
-        lower=0, upper=inf, coords=coords, name="StorageLevelSeasonStart", integer=False
-    )
-    coords = [
-        ds.coords["REGION"],
-        ds.coords["STORAGE"],
-        ds.coords["SEASON"],
-        ds.coords["DAYTYPE"],
-        ds.coords["YEAR"],
-    ]
-    m.add_variables(
-        lower=0, upper=inf, coords=coords, name="StorageLevelDayTypeStart", integer=False
-    )
-    coords = [
-        ds.coords["REGION"],
-        ds.coords["STORAGE"],
-        ds.coords["SEASON"],
-        ds.coords["DAYTYPE"],
-        ds.coords["YEAR"],
-    ]
-    m.add_variables(
-        lower=0, upper=inf, coords=coords, name="StorageLevelDayTypeFinish", integer=False
-    )
-    m.add_variables(lower=0, upper=inf, coords=RSY, name="StorageLowerLimit", integer=False)
-    m.add_variables(lower=0, upper=inf, coords=RSY, name="StorageUpperLimit", integer=False)
-    m.add_variables(
-        lower=0, upper=inf, coords=RSY, name="AccumulatedNewStorageCapacity", integer=False
-    )
     m.add_variables(lower=0, upper=inf, coords=RSY, name="NewStorageCapacity", integer=False)
-    m.add_variables(lower=0, upper=inf, coords=RSY, name="CapitalInvestmentStorage", integer=False)
-    m.add_variables(
-        lower=0, upper=inf, coords=RSY, name="DiscountedCapitalInvestmentStorage", integer=False
-    )
-    m.add_variables(lower=0, upper=inf, coords=RSY, name="SalvageValueStorage", integer=False)
-    m.add_variables(
-        lower=0, upper=inf, coords=RSY, name="DiscountedSalvageValueStorage", integer=False
-    )
-    m.add_variables(
-        lower=0, upper=inf, coords=RSY, name="TotalDiscountedStorageCost", integer=False
-    )
     return m
