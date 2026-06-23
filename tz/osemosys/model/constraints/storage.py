8c22f5f4b6044ae0fd89c56afc2ad7523f2fe251
Joel <93267535+joel1120@users.noreply.github.com>
2026-06-23 14:13:17 +0100

    Revert copilot suggestion

--- tz/osemosys/model/constraints/storage.py
+++ tz/osemosys/model/constraints/storage.py
@@ -261,8 +261,7 @@ def add_storage_constraints(ds: xr.Dataset, m: Model, lex: Dict[str, LinearExpre
         # stack StorageLevel and NetCharge to be used in constraints
         level = m["StorageLevel"].stack(YRTS=["YEAR", "TIMESLICE"])
         net = lex["NetCharge"].stack(YRTS=["YEAR", "TIMESLICE"])
-        level, net = xr.align(level, net, join="left")
-        net = net.fillna(0)
+        
         # mask to filter out first timeslice
         not_first = xr.DataArray(
             np.arange(level.indexes["YRTS"].size) >= 1,
