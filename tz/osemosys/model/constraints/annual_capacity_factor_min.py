a9be8275d11aff26407f1cc077ca7a407049198f
Dan Welsby <161503050+djwels@users.noreply.github.com>
2026-06-29 11:17:18 +0100

    remove availability factor from min_annual_capacity_factor (#186)
    * remove availability factor from min_annual_capacity_factor
    
    * remove ds[AvailabilityFactor from ampl docs as suggested by copilot
    
    * fix CI test persmissions
    
    * fix CI test
    
    * revert ci test changes
    
    * revert pyproject.toml
    
    * Mark repo as safe for git
    
    * update field_serializer for new pydantic version
    
    ---------
    
    Co-authored-by: djwels <dan.welsby@transitionzero.org>
    Co-authored-by: Edward Terpilowski-Gill <edwardterpilowski-gill@TZ-LPT-045.local>
    Co-authored-by: Joel1120 <joelyap1120@hotmail.com>

--- tz/osemosys/model/constraints/annual_capacity_factor_min.py
+++ tz/osemosys/model/constraints/annual_capacity_factor_min.py
@@ -29,12 +29,12 @@ def add_annual_capacity_factor_min_constraints(
     s.t ACF1_TotalAnnualMinCapacityFactor{
         r in REGION, t in TECHNOLOGY, y in YEAR: TotalAnnualMinCapacityFactor[r,t,y] > 0}:
         TotalTechnologyAnnualActivity[r,t,y] >=
-        (GrossCapacity[r,t,y] * AvailabilityFactor[r,t,y] * CapacityToActivityUnit[r,t])
+        (GrossCapacity[r,t,y] * CapacityToActivityUnit[r,t])
         * TotalAnnualMinCapacityFactor[r,t,y];
     ```
     """
     con = lex["TotalTechnologyAnnualActivity"] >= ds["TotalAnnualMinCapacityFactor"] * (
-        lex["GrossCapacity"] * ds["AvailabilityFactor"] * ds["CapacityToActivityUnit"]
+        lex["GrossCapacity"] * ds["CapacityToActivityUnit"]
     )
 
     m.add_constraints(con, name="ACF1_TotalAnnualMinCapacityFactor")
